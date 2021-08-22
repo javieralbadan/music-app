679b3acd2fdfab6178a0045ba54fcf5f0097f1ed
Javier Albadán <javier.albadan@gmail.com>
2021-08-22 13:07:58 -0500

    Including eslint rules. Installing node/sass loaders. Cleaning base components.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,36 @@
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    node: true
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false
-  },
-  extends: [
-    '@nuxtjs',
-    'plugin:nuxt/recommended',
-  ],
-  plugins: [
-  ],
-  // add your custom rules here
-  rules: {}
+	root: true,
+	env: {
+		browser: true,
+		node: true,
+	},
+	parserOptions: {
+		parser: '@babel/eslint-parser',
+		requireConfigFile: false,
+	},
+	extends: [
+		'@nuxtjs',
+		'plugin:nuxt/recommended',
+	],
+	plugins: [
+	],
+	// add your custom rules here
+	rules: {
+		'arrow-parens': ['error', 'always'],
+		'comma-dangle': ['error', 'always-multiline'],
+		indent: ['error', 'tab', {
+			ignoredNodes: ['TemplateLiteral'],
+		}],
+		'max-len': ['error', { code: 120 }],
+		'no-empty-pattern': 'off',
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'object-curly-spacing': ['error', 'always'],
+		'prefer-template': 1,
+		'space-before-function-paren': ['error', 'never'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'template-curly-spacing': 'off',
+		'vue/html-indent': 'off',
+		'vue/singleline-html-element-content-newline': 'off',
+	},
 }
